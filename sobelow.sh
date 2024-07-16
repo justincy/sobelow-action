e2cac5163138ea6e0f2a36e229f3cc3257b95b47
Justin York <justin.york@smartrent.com>
2024-07-16 11:25:26 -0500

    debug

--- sobelow.sh
+++ sobelow.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -l
 
+echo "Begin sobelow.sh"
+
 mix local.hex --force
 mix escript.install hex sobelow --force
+
+echo "After mix install"
 
 ls -al ~
 ls -al ~/.mix
 ls -al ~/.mix/escripts
 
 mix escript
+
+echo "Before sobelow"
 
 if [ "$2" = "false" ]; then
     ~/.mix/escripts/sobelow $1
