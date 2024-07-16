4d0011bf59d9ce33483f43e9802decada457d10f
Justin York <justin.york@smartrent.com>
2024-07-16 11:10:42 -0500

    debug

--- sobelow.sh
+++ sobelow.sh
@@ -3,8 +3,11 @@
 mix local.hex --force
 mix escript.install hex sobelow --force
 
+echo "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
+pwd
+
 if [ "$2" = "false" ]; then
-    ~/.mix/escripts/sobelow $1
+    .mix/escripts/sobelow $1
 else
-    ~/.mix/escripts/sobelow $1 --format sarif >> results.sarif
+    .mix/escripts/sobelow $1 --format sarif >> results.sarif
 fi
