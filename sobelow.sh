61d39007a731891ef1229453f0a5ae2d4bc05a7d
Justin York <justin.york@smartrent.com>
2024-07-16 16:09:22 -0500

    Simplify and fix for umbrella apps

--- sobelow.sh
+++ sobelow.sh
@@ -1,15 +1,13 @@
 #!/bin/sh -l
 
+# Create an empty secrets file to make mix happy.
+mkdir -p config/secrets
+touch config/secrets/dev.exs
+
+# Install sobelow as an escript (meaning we can run it from the cli and don't need to install deps)
 mix local.hex --force
 mix escript.install hex sobelow --force
 
-pwd
-ls -al
-ls -al config
-ls -al config/secrets
-
-if [ "$2" = "false" ]; then
-    ~/.mix/escripts/sobelow $1
-else
-    ~/.mix/escripts/sobelow $1 --format sarif >> results.sarif
-fi
+# Run sobelow
+cd apps/control_room
+~/.mix/escripts/sobelow --config ../../.sobelow-conf --format sarif >> results.sarif
